21654831d2d41ba7599928e0fb8a093832653f63
ZYMa <317958662@qq.com>
2021-08-28 18:57:42 +0800

    Create gen_sent_weights.py

---
+++ gendata/gen_sent_weights.py
@@ -0,0 +1,73 @@
+import sys, os, argparse, json
+parentdir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.insert(0, parentdir)
+import torch
+from torch.utils.data import DataLoader
+from transformers import BertTokenizer, BertModel
+from models import BertForCLSWeight
+from mydatasets import SentenceGenCLSWeightDataset
+from tqdm import tqdm
+from utils.utils import *
+parser = argparse.ArgumentParser()
+parser.add_argument("--per_gpu_test_batch_size", default=64, type=int, help="The batch size.")
+parser.add_argument("--model_path", default="/home/yutao_zhu/BertModel/", type=str, help="The path to save log.")
+parser.add_argument("--sentence_file_path", default="/home/yutao_zhu/BertModel/", type=str, help="The path to save log.")
+parser.add_argument("--dataset_script_dir", default="/home/yutao_zhu/BertModel/", type=str, help="The path to save log.")
+parser.add_argument("--dataset_cache_dir", default="/home/yutao_zhu/BertModel/", type=str, help="The path to save log.")
+parser.add_argument("--output_file_path", default="/home/yutao_zhu/BertModel/", type=str, help="The path to save log.")
+parser.add_argument("--cpu", action="store_true")
+args = parser.parse_args()
+if args.cpu:
+    device = torch.device("cpu")
+    args.test_batch_size = args.per_gpu_test_batch_size * 1
+else:
+    device = torch.device("cuda:0")
+    args.test_batch_size = args.per_gpu_test_batch_size * torch.cuda.device_count()
+print(args)
+predict_data = args.sentence_file_path
+tokenizer = BertTokenizer.from_pretrained(args.model_path)
+
+def predict(model, X_test):
+    model.eval()
+    test_dataset = SentenceGenCLSWeightDataset(X_test, 512, tokenizer, args.dataset_script_dir, args.dataset_cache_dir)
+    test_dataloader = DataLoader(test_dataset, batch_size=args.test_batch_size, shuffle=False, num_workers=1)
+    y_pred = []
+    y_label = []
+    attention_weights = []
+    fw = open(args.output_file_path, 'w')
+    with torch.no_grad():
+        epoch_iterator = tqdm(test_dataloader, ncols=120, leave=False)
+        for i, test_data in enumerate(epoch_iterator):
+            with torch.no_grad():
+                for key in test_data.keys():
+                    test_data[key] = test_data[key].to(device)
+            attentions = model(test_data)
+            output_attention_weights =  attentions.data.cpu().numpy().tolist()
+            attention_weights += output_attention_weights
+    assert len(attention_weights) == len(test_dataset.passage_dataset)
+    print("start writing.........................")
+    for i in tqdm(range(len(attention_weights))):
+        sid = test_dataset.passage_dataset[i]['sid']
+        stext = test_dataset.passage_dataset[i]['sentence_tokens']
+        anos =  test_dataset.passage_dataset[i]['ano']
+        pnos =  test_dataset.passage_dataset[i]['pno']
+        slen = len(stext)
+        cls_attention = attention_weights[i][1:slen+1]
+        if len(cls_attention) != slen:
+            stext = stext[:500]
+            cls_attention =  attention_weights[i][1:500+1]
+        att_text = []
+        for st, sa in zip(stext, cls_attention):
+            att_text += [[st, sa]]
+        fw.write(json.dumps({"att_text": att_text, "sid":sid, "ano":anos, "pno":pnos, "sentence_tokens":stext}, ensure_ascii=False)+"\n")
+    fw.close()
+    return y_pred, y_label
+    
+if __name__ == '__main__':
+    set_seed()
+    bert_model = BertModel.from_pretrained(args.model_path,output_attentions=True)
+    model = BertForCLSWeight(bert_model)
+    model = model.to(device)
+    model = torch.nn.DataParallel(model)
+    predict(model, predict_data)
+
